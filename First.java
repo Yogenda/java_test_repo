f5137e1cf33ee15c95fbad7ab43a7f0129d300a4
Yogenda <yogendrabire61@gmail.com>
2024-09-10 18:59:50 +0530

    Code added to comment

--- First.java
+++ First.java
@@ -5,6 +5,7 @@ class First{
         int a  = sc.nextInt();
         System.out.println("Value of A is:"+a);
         //enter the message for input in b
+        System.out.println("Enter the valus of B:")
         int b  = sc.nextInt();
         int c = a+b;
         System.out.println("Sum of A and B is: "+c);
