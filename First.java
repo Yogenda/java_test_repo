722c2485be3c018077b3dec000e33cd2dcdfd1d4
Yogendra <37645980+Yogenda@users.noreply.github.com>
2024-09-10 18:42:42 +0530

    int a added for input

--- First.java
+++ First.java
@@ -2,5 +2,7 @@ class First{
     public static void main(string args[]){
         System.out.println("Hello world");
         Scanner sc = new Scanner(System.in);
+        int a  = sc.nextInt();
+        System.out.println("Value of A is:"+a);
     }
 }
